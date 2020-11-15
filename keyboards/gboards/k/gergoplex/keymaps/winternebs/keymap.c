398fc3e55968ef207965c63873e1a1b361770b34
winterNebs <winterNebs@gmail.com>
2020-11-15 18:12:11 -0500

    fix combos

--- keyboards/gboards/k/gergoplex/keymaps/winternebs/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/winternebs/keymap.c
@@ -8,6 +8,7 @@
 
 #include QMK_KEYBOARD_H
 #include "g/keymap_combo.h"
+#include "keymap.h"
 
 enum layers{
     _ALPHA = 0,   // default
@@ -17,17 +18,6 @@ enum layers{
 enum custom_keycodes {
     ENDW  = SAFE_RANGE
 };
-
-#define RAISE LT(_RAISE, KC_ENT)
-#define LOWER LT(_LOWER, KC_TAB)
-#define HOME_A GUI_T(KC_A)
-#define HOME_S ALT_T(KC_S)
-#define HOME_H CTL_T(KC_H) 
-#define HOME_T SFT_T(KC_T) 
-#define HOME_N SFT_T(KC_N) 
-#define HOME_E CTL_T(KC_E)
-#define HOME_O ALT_T(KC_O)
-#define HOME_I GUI_T(KC_I)
 
 /* Combomap
  *
