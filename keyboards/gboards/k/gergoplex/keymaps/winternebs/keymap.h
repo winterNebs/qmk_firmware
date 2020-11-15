f3c4e4a5533d92e66f4de7e83deb15d6bf879235
winterNebs <winterNebs@gmail.com>
2020-11-15 18:25:52 -0500

    fix build

---
+++ keyboards/gboards/k/gergoplex/keymaps/winternebs/keymap.h
@@ -0,0 +1,12 @@
+#include QMK_KEYBOARD_H 
+#define RAISE LT(_RAISE, KC_ENT)
+#define LOWER LT(_LOWER, KC_TAB)
+#define HOME_A GUI_T(KC_A)
+#define HOME_S ALT_T(KC_S)
+#define HOME_H CTL_T(KC_H) 
+#define HOME_T SFT_T(KC_T) 
+#define HOME_N SFT_T(KC_N) 
+#define HOME_E CTL_T(KC_E)
+#define HOME_O ALT_T(KC_O)
+#define HOME_I GUI_T(KC_I)
+
