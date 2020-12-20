c20135fe9a1cdfeca60f9ad526a486b6a9415606
winterNebs <winterNebs@gmail.com>
2020-12-19 20:22:54 -0500

    set up userspace

---
+++ users/winternebs/rules.mk
@@ -0,0 +1,14 @@
+#----------------------------------------------------------------------------
+# make gergo:germ:dfu
+# Make sure you have dfu-programmer installed!
+#----------------------------------------------------------------------------
+# Firmware options
+SRC += winternebs.c
+MOUSEKEY_ENABLE 					= yes
+
+#Debug options
+#VERBOSE 		 							= yes
+#DEBUG_MATRIX_SCAN_RATE   	= no
+#DEBUG_MATRIX		 					= yes
+#CONSOLE_ENABLE						= yes
+
