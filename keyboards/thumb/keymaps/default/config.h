0a0e5cd96570b40eb2bbe08a6f735f2a1774c8a2
winterNebs <winterNebs@gmail.com>
2024-09-28 14:37:19 -0400

    wasd mapping for joystick

--- keyboards/thumb/keymaps/default/config.h
+++ keyboards/thumb/keymaps/default/config.h
@@ -1,4 +1,5 @@
 #pragma once
+#define FORCE_NKRO
 // Min 0, max 32
 #define JOYSTICK_BUTTON_COUNT 18
 // Min 0, max 6: X, Y, Z, Rx, Ry, Rz
@@ -6,5 +7,5 @@
 // Min 8, max 16
 #define JOYSTICK_AXIS_RESOLUTION 12
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1500U // Timeout window in ms in which the double tap can occur.
 
--- keyboards/thumb/keymaps/default/keymap.c
+++ keyboards/thumb/keymaps/default/keymap.c
@@ -1,74 +1,131 @@
-// Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 #include "analog.h"
 
 joystick_config_t joystick_axes[JOYSTICK_AXIS_COUNT] = {
-    JOYSTICK_AXIS_IN(GP26, 0, 512, 1023),
-    JOYSTICK_AXIS_IN(GP27, 0, 512, 1023),
+    JOYSTICK_AXIS_IN(GP26, 0, 512, 1023), JOYSTICK_AXIS_IN(GP27, 0, 512, 819), // bad print LOL
 };
-enum layers {
-    _DEF,
-    _DL,
-    _RL,
-    _NUM,
-};
+enum layers { _DEF = 0, _DL, _RL, _NUM };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
     /*
-     * ┌───┬───┬───┬───┬───┐
-     * │Esc│ Q │ W │ E │ R │
-     * ├───┼───┼───┼───┼───┤   W
-     * │Shf│ A │ S │ D │ F │ A   D
-     * ├───┼───┼───┼───┼───┤   S
-     * │Ctl│ Z │ X │ C │ V │
-     * └───┼───┼───┼───┼───┤
-     *     │Tab│NUM│Spc│   │
-     *     └───┴───┴───┴───┘
+     *     ┌───┬───┬───┬───┐
+     * ┌───┤ Q │ W │ E │ R │
+     * │TAB├───┼───┼───┼───┤   W
+     * ├───┤ A │ S │ D │ F │ A   D
+     * │Shf├───┼───┼───┼───┤   S
+     * ├───┤ Z │ X │ C │ V │
+     * │Ctl├───┼───┼───┼───┘
+     * └───┤ESC│   │NUM│   ENT
+     *     └───┴───┴───┘
      */
-    [_DEF] = LAYOUT(KC_ESC, KC_Q, KC_W, KC_E, KC_R,
+    [_DEF] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R,
                 KC_LSFT, KC_A, KC_S, KC_D, KC_F,
                 KC_LCTL, KC_Z, KC_X, KC_C, KC_V,
-                      KC_TAB, MO(_NUM), KC_SPC, KC_ENT),
+                      KC_ESC, KC_SPC, MO(_NUM), KC_ENT),
     /*
      * ┌───┬───┬───┬───┬───┐
-     * │Esc│ 1 │ 2 │ 3 │ R │
+     * │TAB│ Q │ 2 │ E │ R │
      * ├───┼───┼───┼───┼───┤   W
-     * │Shf│   │   │SPC│ F │ A   D
+     * │Shf│ 1 │SPC│ 3 │ F │ A   D
      * ├───┼───┼───┼───┼───┤   S
      * │Ctl│ Z │ X │ C │ V │
      * └───┼───┼───┼───┼───┤
-     *     │Tab│NUM│Spc│   │
+     *     │ESC│   │NUM│   │
      *     └───┴───┴───┴───┘
      */
-    [_DL] = LAYOUT(KC_ESC, KC_Q, KC_W, KC_E, KC_R,
-                KC_LSFT, KC_A, KC_S, KC_D, KC_F,
+    [_DL] = LAYOUT(KC_TAB, KC_Q, KC_2, KC_E, KC_R,
+                KC_LSFT, KC_1, KC_SPC, KC_3, KC_F,
                 KC_LCTL, KC_Z, KC_X, KC_C, KC_V,
-                      KC_TAB, MO(_NUM), KC_SPC, KC_SPC),
+                      KC_ESC, _______, _______,_______),
     /*
-     * ┌───┬───┬───┬───┬───┐
-     * │Esc│ 1 │ 2 │ 3 │ R │
-     * ├───┼───┼───┼───┼───┤   W
-     * │Shf│   │   │SPC│ F │ A   D
-     * ├───┼───┼───┼───┼───┤   S
-     * │Ctl│ Z │ X │ C │ V │
-     * └───┼───┼───┼───┼───┤
-     *     │Tab│NUM│Spc│   │
-     *     └───┴───┴───┴───┘
+     *     ┌───┬───┬───┬───┐
+     * ┌───┤ ^ │R_T│R_S│ △ │
+     * │ ≡ ├───┼───┼───┼───┤  LS_X
+     * ├───┤ v │L_T│L_S│ ○ │
+     * │ < ├───┼───┼───┼───┤  LS_Y
+     * ├───┤ > │RIN│ ◻ │ x │
+     * │⌥  ├───┼───┼───┼───┘
+     * └───┤ ⇒ │ ⌂ │NUM│   LIN
+     *     └───┴───┴───┘
+     *     Controller
+     *      L_SHLD      R_SHLD
+     *      L_TRGR      R_TRGR
+     *
+     *      ⌥  ≡
+     *      ⌂ ⇒
+     *
+     *      DP_U DP_R     △
+     *      DP_L DP_D   ◻   ○
+     *                    x
+     *
+     *      LS_X        RS_X
+     *      LS_IN       RS_IN
+     *      LS_Y        RS_Y
+     *
      */
     [_RL] = LAYOUT(JS_0, JS_1, JS_2, JS_3, JS_4,
                     JS_5, JS_6, JS_7, JS_8, JS_9,
                     JS_10, JS_11, JS_12, JS_13, JS_14,
-                    JS_15, MO(_NUM), JS_16, JS_17),
+                    JS_15, JS_16, _______, JS_17),
 
     [_NUM] = LAYOUT(KC_1, KC_2, KC_3, KC_4, KC_5,
-                _______, _______, _______, _______, _______,
+                DF(_DL), _______, _______, _______, _______,
                 DF(_DEF), _______, _______, _______, _______,
-                      DF(_RL), MO(_NUM), KC_SPC, KC_SPC)
+                      DF(_RL),_______, _______, _______ )
 
     // clang-format on
 };
+// Firmware inspired by joshinator.de
+bool    wasdMode = false;
+int16_t xPos     = 0;
+int16_t yPos     = 0;
 
+#define _MID 512
+#define _DEAD 75
+#define _DOWN (_MID - _DEAD)
+#define _UP (_MID + _DEAD)
+
+void matrix_scan_user(void) {
+    if (wasdMode) {
+        // W & S
+        xPos = analogReadPin(GP26);
+        yPos = analogReadPin(GP27);
+
+        // DOWN --- DEAD ZONE --- UP
+        if (yPos >= _UP) {
+            register_code(KC_W);
+            unregister_code(KC_S);
+        } else if (yPos <= _DOWN) {
+            register_code(KC_S);
+            unregister_code(KC_W);
+        } else {
+            unregister_code(KC_S);
+            unregister_code(KC_W);
+        }
+        if (xPos >= _UP) {
+            register_code(KC_A);
+            unregister_code(KC_D);
+        } else if (xPos <= _DOWN) {
+            register_code(KC_D);
+            unregister_code(KC_A);
+        } else {
+            unregister_code(KC_D);
+            unregister_code(KC_A);
+        }
+    }
+}
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    switch (biton32(state)) {
+        case _DL:
+            wasdMode = true;
+            break;
+        default:
+            wasdMode = false;
+            break;
+    }
+    return state;
+}
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {}
